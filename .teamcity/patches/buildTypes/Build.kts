c0d77fc1846a55b6804f0c0400434b2e9ab3fc8d
mitesh.gangaramani <mitesh.gangaramani@TeamCity>
2020-05-20 13:27:51 +0530

    TeamCity change in 'Kotlin Hello World Test' project: triggers of 'Build' build configuration were updated

--- .teamcity/patches/buildTypes/Build.kts
+++ .teamcity/patches/buildTypes/Build.kts
@@ -1,7 +1,6 @@
 package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.VcsTrigger
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.ui.*
 
@@ -12,14 +11,9 @@ accordingly, and delete the patch script.
 */
 changeBuildType(RelativeId("Build")) {
     triggers {
-        val trigger1 = find<VcsTrigger> {
+        remove {
             vcs {
             }
-        }
-        trigger1.apply {
-            branchFilter = ""
-            perCheckinTriggering = true
-            enableQueueOptimization = false
         }
     }
 }
