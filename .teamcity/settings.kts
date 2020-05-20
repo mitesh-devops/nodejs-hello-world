e2c7d91e4daa48ee9bd188061d61fce5d3e532cc
mitesh.gangaramani <mitesh.gangaramani@TeamCity>
2020-05-20 16:28:54 +0530

    TeamCity change in 'Mitesh DSL' project: Versioned settings configuration updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,78 +1,28 @@
-package _Self.buildTypes
-
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.commitStatusPublisher
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.pullRequests
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dockerCommand
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
-import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
-object BuildTest : BuildType({
-    name = "Build Test"
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
 
-    }
-    steps {
-        script {
-            name = "random test"
-            enabled = false
-            scriptContent = "service docker status"
-        }
-        step {
-            name = "install dependency"
-            type = "jonnyzzz.nvm"
-            enabled = false
-            param("version", "13.6.0")
-        }
-        step {
-            name = "Install modules and run test"
-            type = "jonnyzzz.npm"
-            enabled = false
-            param("npm_commands", "install")
-        }
-        dockerCommand {
-            name = "Docker Build image"
-            enabled = false
-            executionMode = BuildStep.ExecutionMode.RUN_ON_FAILURE
-            commandType = build {
-                source = file {
-                    path = "Dockerfile"
-                }
-                namesAndTags = "gcr.io/%PROJECT_ID%/node-app:dev-%build.counter%"
-                commandArgs = "--pull"
-            }
-            param("dockerImage.platform", "linux")
-        }
-        script {
-            name = "service account authentication"
-            enabled = false
-            scriptContent = """
-                echo '%GCLOUD_KEY%' > ./new-file.json
-                gcloud auth activate-service-account --key-file=./new-file.json --project=%PROJECT_ID%
-                gcloud container clusters get-credentials %CLUSTER_NAME% --zone us-central1-c --project %PROJECT_ID%
-                kubectl config current-context
-                kubectl config set-context --current --namespace=mitesh
-                gcloud auth configure-docker
-            """.trimIndent()
-        }
-        dockerCommand {
-            name = "Docker Push"
-            enabled = false
-            commandType = push {
-                namesAndTags = "gcr.io/%PROJECT_ID%/node-app:dev-%build.counter%"
-                removeImageAfterPush = false
-            }
-        }
-        script {
-            name = "deploy image to kubernetes"
-            enabled = false
-            scriptContent = "IMAGETAG='%IMAGE_TAG%' node deploy-apps.js"
-        }
-    }
-    triggers {
-        vcs {
-            enabled = false
-            branchFilter = ""
-        }
-    }
- }
-})
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2019.2"
+
+project {
+}
